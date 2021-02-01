834aac39a187d6d680a757481d1b54bf6fd4e056
Carlos Huerta <charlieshuerta@gmail.com>
2021-02-01 15:42:44 -0600

    Modificación de archivos y creación de nuevos archivos

---
+++ holamundo/js/12-ejercicio2.js
@@ -0,0 +1,24 @@
+'use strict'
+/*
+UTLIZANDO UN BUCLE, MOSTRAR LA SUMA Y LA MEDIA DE LOS NUMEROS INTRODUCIDOS HASTA INTRODUCIR UN NUEMRO NEGATIVO Y AHI MOSTRAR EL RESULTADO
+*/
+
+var suma = 0;
+var contador = 0;
+
+do{
+    var numero = parseInt (prompt("Introduce numeros hasta que ingreses un negativo", 0));
+
+    if(isNaN(numero)){
+        numero = 0
+    }else if(numero >= 0){
+        suma = suma + numero;
+        //suma += numero;
+        contador++;
+    }
+
+    console.log(suma +' | '+contador);
+}while(numero >= 0);
+
+alert("La suma de todos los numeros es: "+suma);
+alert("La media de todos los numeros es: "+(suma/contador));
