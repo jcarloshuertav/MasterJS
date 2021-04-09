e52ef54054365c1ef8283760c8f723caa763b128
Carlos Huerta <charlieshuerta@gmail.com>
2021-04-09 13:44:17 -0500

    Modificación y creación de archivos

--- holamundo/js/19-funciones.js
+++ holamundo/js/19-funciones.js
@@ -5,22 +5,31 @@
  * UNA FUNCION ES UNA AGRUPACION REUTILIZABLE DE UN CONJUNTO DE INSTRUCCIONES
  */
 
+function porConsola(num1, num2){
+    console.log("SUMA: "+(num1+num2));
+    console.log("RESTA: "+(num1-num2));
+    console.log("MULTIPLICACION: "+(num1*num2));
+    console.log("DIVISION: "+(num1/num2));
+    console.log("* * * * * * * * * *");
+}
+
+function porPantalla(num1, num2){
+    document.write("SUMA: "+(num1+num2)+"<br>");
+    document.write("RESTA: "+(num1-num2)+"<br>");
+    document.write("MULTIPLICACION: "+(num1*num2)+"<br>");
+    document.write("DIVISION: "+(num1/num2)+"<br>");
+    document.write("* * * * * * * * * *<br>");
+}
+
 //DEFINO LA FUNCION
 function calculadora(num1, num2, mostrar = false){
     //CONJUNTO DE INSTRUCCIONES A EJECUTAR
     if(mostrar == false){
-        console.log("SUMA: "+(num1+num2));
-        console.log("RESTA: "+(num1-num2));
-        console.log("MULTIPLICACION: "+(num1*num2));
-        console.log("DIVISION: "+(num1/num2));
-        console.log("* * * * * * * * * *");
+        porConsola(num1, num2);
     }else{
-        document.write("SUMA: "+(num1+num2)+"<br>");
-        document.write("RESTA: "+(num1-num2)+"<br>");
-        document.write("MULTIPLICACION: "+(num1*num2)+"<br>");
-        document.write("DIVISION: "+(num1/num2)+"<br>");
-        document.write("* * * * * * * * * *<br>");
+        porPantalla(num1, num2);
     }
+    return true;
 }
 //INVOCAR A LA FUNCION
 calculadora(2,6, true);
---
+++ holamundo/js/20-parametros.js
@@ -0,0 +1,12 @@
+'use strict'
+//PARAMETROS REST Y SPREAD
+function listadoFrutas(fruta1, fruta2, ...resto_frutas){
+    console.log("Fruta 1: ",fruta1);
+    console.log("Fruta 2: ",fruta2);
+    console.log(resto_frutas);
+}
+
+listadoFrutas("Naranja", "Manzana", "Pera", "Melon", "Coco");
+
+var frutas = ["Naranja", "Manzana"];
+listadoFrutas(...frutas, "Pera", "Melon", "Coco");
