f012139a95fb4f38f8d101c916a62fed34a69d8d
Carlos Huerta <charlieshuerta@gmail.com>
2021-08-27 13:20:23 -0500

    Ejercicio 22

---
+++ holamundo/js/22-ambito.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function holaMundo(texto) {
+    var hola_mundo = "Texto dentro de la función";
+
+    console.log(texto);
+    console.log(numero.toString());
+    console.log(hola_mundo);
+}
+
+var numero = 21;
+var texto = "Hola soy una variable global";
+holaMundo(texto);
