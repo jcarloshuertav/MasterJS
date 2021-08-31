9f89e5949a18676074c6f58b04ebf18e68b15134
Carlos Huerta <charlieshuerta@gmail.com>
2021-08-31 13:02:53 -0500

    Ejercicio 23

---
+++ holamundo/js/23-funciones-texto.js
@@ -0,0 +1,20 @@
+'use strict'
+
+//TRANSFORMACION DE TEXTOS
+var numero = 21;
+var txt1 = "Hola";
+var txt2 = "Mundo";
+
+var dato = numero.toString();
+    dato = txt1.toUpperCase();
+    dato = txt2.toLowerCase();
+
+console.log(dato);
+
+//CALCULAR LONGITUD
+var nombre = "Carlos";
+console.log(nombre.length);
+
+//CONCATENAR TEXTOS
+var txtTotal = txt1.concat(" "+txt2);
+console.log(txtTotal);
